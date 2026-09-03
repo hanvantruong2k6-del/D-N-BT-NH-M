91951f0c37355fe56fcfff12326063e7ecfa3776
trangdtt870938 <trangdtt870938@ut.edu.vn>
2026-09-03 16:25:31 +0700

    Add files via upload

---
+++ disjointset.py
@@ -0,0 +1,27 @@
+class DisjointSet:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    def union(self, a, b):
+        root_a = self.find(a)
+        root_b = self.find(b)
+        if root_a == root_b:
+            return False
+        if self.rank[root_a] < self.rank[root_b]:
+            self.parent[root_a] = root_b
+        elif self.rank[root_a] > self.rank[root_b]:
+            self.parent[root_b] = root_a
+        else:
+            self.parent[root_b] = root_a
+            self.rank[root_a] += 1
+        return True
+
+ds = DisjointSet(5)
+ds.union(0, 1)
+ds.union(1, 2)
+print("Goc cua 0:", ds.find(0))
+print("Goc cua 2:", ds.find(2))
